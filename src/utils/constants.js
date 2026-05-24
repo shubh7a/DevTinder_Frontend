18c887e518cca8a9b4a11c93187938064be8338d
Shubham Kumar Singh <shubh7a@gmail.com>
2026-05-24 15:38:05 +0530

    connected production backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = "https://devtinder-backend-n0e0.onrender.com";
